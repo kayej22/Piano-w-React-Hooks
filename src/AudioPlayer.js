1af8731e8731adf6b889aadc6ded3d8bb9412722
Joel Kaye <kayej22@hotmail.com>
2021-04-29 09:38:00 -0600

    working on audio player

---
+++ src/AudioPlayer.js
@@ -0,0 +1,14 @@
+import SoundFontPlayer from "soundfont-player";
+import AudioContext from "./AudioContext";
+
+const NullSoundFontPlayerNoteAudio = {
+  stop() {}
+};
+
+const NullSoundFontPlayer = {
+  play() {
+    return NullSoundFontPlayerNoteAudio;
+  }
+};
+
+const AudioPlayer = () =>
