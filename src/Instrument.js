c12b68991175b51b7561f6d7959e014365306cf1
Joel Kaye <kayej22@hotmail.com>
2021-05-05 09:05:23 -0600

    setting up isAccidentalNote file

--- src/Instrument.js
+++ src/Instrument.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import InstrumentAudio from "./Keyboard/InstrumentAudio"
 import getNotesBetween from "./utils/getNotesBetween";
 
