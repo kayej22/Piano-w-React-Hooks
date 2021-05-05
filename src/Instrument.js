0c9fe0f4da72bf91ab65bb0cd73c0ff8337979e7
Joel Kaye <kayej22@hotmail.com>
2021-05-05 09:37:36 -0600

    instrument.js update

--- src/Instrument.js
+++ src/Instrument.js
@@ -1,14 +1,50 @@
 import React, { Fragment, useState } from "react";
 import InstrumentAudio from "./Keyboard/InstrumentAudio"
 import getNotesBetween from "./utils/getNotesBetween";
+import isAccidentalNote from "./utils/isAccidentalNote"
 
-const Instrument = ({ instrumentName, startNote, endNote }) => {
+const Instrument = ({ 
+  instrumentName,
+  startNote,
+  endNote,
+  renderPianoKey,
+  keyboardMap
+ }) => {
   const notes = getNotesBetween(startNote, endNote);
+
+  const [state, setState] = useState({
+    notesPlaying: []
+  });
+
+  const onPlayNoteStart = note => {
+    setState({ ...state, notesPlaying: [...state.notesPlaying, note] });
+  };
+
+  const onPlayNoteEnd = note => {
+    setState({
+      ...state,
+      notesPlaying:state.notesPlaying.filter(
+        notePlaying => notePlaying !== note
+      )
+    })
+  };
+  
   return (
     <Fragment>
       {notes.map(note => {
-        return <Fragment>Note is : {note}</Fragment>
-      })}
+        return (
+        <Fragment key={note}>
+          {renderPianoKey({
+            note,
+            isAccidentalNote: isAccidentalNote(note),
+            isNotePlaying: state.notesPlaying.includes(note),
+            startPlayingNote: () => onPlayNoteStart(note),
+            stopPlayingNote: () => onPlayNoteEnd(note),
+            keyboardShortcut: getKeyboardShortcutsForNote(keyboardMap, note) 
+          })}
+        </Fragment>
+      );
+    })}
       <InstrumentAudio />
     </Fragment>
   );
